4cbabb0d6772cf03fb9dc7e8b643b2d5ce17988f
Olivier Sallou <olivier.sallou@gmail.com>
2016-04-26 17:00:57 +0200

    use v1.0.4

--- Library/Formula/cassiopee.rb
+++ Library/Formula/cassiopee.rb
@@ -2,9 +2,11 @@ require 'formula'
 
 class Cassiopee < Formula
   homepage 'http://osallou.github.io/cassiopee-c/'
-  url "https://github.com/osallou/cassiopee-c/archive/1.0.3.tar.gz"
-  version "1.0.3"
-  sha256 "a0cf78a3bc0d99d046ac0af5e7e486105af27a203a79ee673aa14de478943394"
+  #url "https://github.com/osallou/cassiopee-c/archive/1.0.3.tar.gz"
+  url "https://github.com/osallou/cassiopee-c/archive/1.0.4.tar.gz"
+  version "1.0.4"
+  #sha256 "a0cf78a3bc0d99d046ac0af5e7e486105af27a203a79ee673aa14de478943394"
+  sha256 "b62a7d4512c052ef524179373723b05c0dbdedb8e37eea126fd48c34be6be300"
 
   depends_on 'cmake' => :build
   depends_on 'boost'
